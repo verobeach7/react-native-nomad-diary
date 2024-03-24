16927d1b6c5712c735bb0c14639fe1fffcf34928
verobeach7 <60215757+verobeach7@users.noreply.github.com>
2024-03-24 13:07:38 +0900

    React Native Master: Diary App(Realm SDK: MongoDB)

--- App.js
+++ App.js
@@ -1,12 +1,61 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
+import Realm from "realm";
+import * as SplashScreen from "expo-splash-screen";
+import React, { useCallback, useEffect, useState } from "react";
+import { View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import Navigator from "./navigator";
 
+// Keep the splash screen visible while we fetch resources
+SplashScreen.preventAutoHideAsync();
+
+const FeelingSchema = {
+  name: "Feeling",
+  properties: {
+    _id: "int",
+    emotion: "string",
+    message: "string",
+  },
+  primaryKey: "_id",
+};
+
 export default function App() {
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  async function startLoading() {
+    try {
+      // realm 데이터베이스 접근을 모든 스크린과 공유해야 함
+      const realm = await Realm.open({
+        path: "diaryDb",
+        schema: [FeelingSchema],
+      });
+      // console.log("realm", realm); // 연결 여부 확인 위해 사용
+      // setRealm(realm);
+    } catch (e) {
+      console.log(e);
+    } finally {
+      setAppIsReady(true);
+    }
+  }
+
+  useEffect(() => {
+    startLoading();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
+
   return (
-    <NavigationContainer>
-      <Navigator />
-    </NavigationContainer>
+    <View style={{ flex: 1 }} onLayout={onLayoutRootView}>
+      <NavigationContainer>
+        <Navigator />
+      </NavigationContainer>
+    </View>
   );
 }
